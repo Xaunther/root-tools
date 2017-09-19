87fe168dfee5c56fae5c7a37287b3db412a76db0
Alejandro Alfonso Albero <alejandro.alfonso.albero@cern.ch>
2017-09-19 16:46:45 +0200

    Add TISTOS script and initializer

--- rootinit.C
+++ rootinit.C
@@ -2,6 +2,8 @@ void rootinit()
 {
   //Utilities from independent sources
   gROOT->ProcessLine(".L ./Functions/BifurcatedCB.cxx+");
+  //Personal tools not depending on anything external
+  gROOT->ProcessLine(".L ./Functions/TISTOS.cxx+");
   //Miscellaneous functions, must be loaded in top of all personal functions
   gROOT->ProcessLine(".L ./Functions/misc.cxx+");  
   //Personal codes
