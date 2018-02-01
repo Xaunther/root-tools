4289ba2e4ec0acd181a91078f201e0b8e3afbe63
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-02-01 11:44:29 +0100

    Different cutset

--- Create_Cuts.C
+++ Create_Cuts.C
@@ -5,19 +5,23 @@
 #include <sstream>
 using namespace std;
 
+#define Nvars 6
+
 void Create_Cuts()
 {
   ofstream fout;
   stringstream ssfile;
-  int ii[4];
+  int ii[Nvars];
   int i=0;
   int n=0;
-  int CutValue[4][16]=
+  int CutValue[Nvars][6]=
     {
-      {15, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
-      {5, 1, 2, 4, 6, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {5, 1, 2, 4, 6, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-      {5, 1, 2, 4, 6, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {5, 832, 837, 842, 847, 852},
+      {5, 927, 932, 937, 942, 947},
+      {5, 3000, 3400, 3700, 4000, 4400},
+      {5, 486, 488, 490, 492, 494},
+      {5, 501, 503, 505, 507, 509},
+      {3, 5, 6, 7}
     };
   for(ii[0]=0;ii[0]<CutValue[0][0];ii[0]++)
     {
@@ -27,15 +31,24 @@ void Create_Cuts()
 	    {
 	      for(ii[3]=0;ii[3]<CutValue[3][0];ii[3]++)
 		{
-		  ssfile.str("");
-		  ssfile << "Cuts/PIDCuts" << n << ".txt";
-		  fout.open(ssfile.str().c_str());
-		  fout << "BDT_response > 0." << CutValue[0][ii[0]+1] << endl
-		       << "piminus_ProbNNpi*(1-piminus_ProbNNp)*(1-piminus_ProbNNk) > 0." << CutValue[1][ii[1]+1] << endl
-		       << "piplus_ProbNNpi*(1-piplus_ProbNNp)*(1-piplus_ProbNNk) > 0." << CutValue[2][ii[2]+1] << endl
-		       << "piplus0_ProbNNpi*(1-piplus0_ProbNNp)*(1-piplus0_ProbNNk) > 0." << CutValue[3][ii[3]+1] << endl;
-		  n++;
-		  fout.close();
+		  for(ii[4]=0;ii[4]<CutValue[4][0];ii[4]++)
+		    {
+		      for(ii[5]=0;ii[5]<CutValue[5][0];ii[5]++)
+			{
+			  ssfile.str("");
+			  ssfile << "Cuts/PIDCuts" << n << ".txt";
+			  fout.open(ssfile.str().c_str());
+			  fout << "piplus_TRACK_Type == 3" << endl
+			       << "piminus_TRACK_Type == 3" << endl
+			       << "B_M > 4300 && B_M < 6300" << endl
+			       << "Kst_892_plus_M > " << CutValue[0][ii[0]+1] <<" && Kst_892_plus_M < " << CutValue[1][ii[1]+1] << endl
+			       << "B_PT > " << CutValue[2][ii[2]+1] << endl
+			       << "KS0_M > " << CutValue[3][ii[3]+1] << " && KS0_M < " << CutValue[4][ii[4]+1] << endl
+			       << "B_DIRA_OWNPV > 0.999" << CutValue[5][ii[5]+1] << endl;
+			  n++;
+			  fout.close();
+			}
+		    }
 		}
 	    }
 	}
