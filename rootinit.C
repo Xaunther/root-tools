6721dc4b8975946e1cc05586fa873a22e34cdb9c
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-02-27 18:01:08 +0100

    rootinit now initializes on selected path

--- rootinit.C
+++ rootinit.C
@@ -1,43 +1,45 @@
+#include <string>
 void rootinit()
 {
+  std::string path = "/afs/cern.ch/work/a/aalfonso/private/";
   //First of all come the personal constants
   gROOT->ProcessLine(".L ./Dictionaries/Constants.cxx+");
   gROOT->ProcessLine(".L ./Dictionaries/Names.cxx+");
   //Utilities from independent sources
-  gROOT->ProcessLine(".L ./Functions/BifurcatedCB.cxx+");
+  gROOT->ProcessLine((".L "+path+"Functions/BifurcatedCB.cxx+").c_str());
   //Personal tools not depending on anything external
-  gROOT->ProcessLine(".L ./Functions/TISTOS.cxx+");
+  gROOT->ProcessLine((".L "+path+"Functions/TISTOS.cxx+").c_str());
   //Miscellaneous functions, must be loaded in top of all personal functions
-  gROOT->ProcessLine(".L ./Functions/misc.cxx+");  
+  gROOT->ProcessLine((".L "+path+"Functions/misc.cxx+").c_str());
   //Personal codes
-  gROOT->ProcessLine(".L ./Functions/Fits.cxx+");
+  gROOT->ProcessLine((".L "+path+"Functions/Fits.cxx+").c_str());
 
   //Finally I can load the user-run scripts, which depend on the libraries already loaded
   //This away when running ROOT interactively I have some help on how to use them (not compiled!)
 
-  gROOT->ProcessLine(".L ./root/AddBranch.C+");
-  gROOT->ProcessLine(".L ./root/BDTApply.C+");
-  gROOT->ProcessLine(".L ./root/BDTCut.C+");
-  gROOT->ProcessLine(".L ./root/BDTMerit.C+");
-  gROOT->ProcessLine(".L ./root/BDTTrain.C+");
-  gROOT->ProcessLine(".L ./root/CompareVar.C+");
-  gROOT->ProcessLine(".L ./root/Compare.C+");
-  gROOT->ProcessLine(".L ./root/Create_Cuts.C+");
-  gROOT->ProcessLine(".L ./root/CutEff.C+");
-  gROOT->ProcessLine(".L ./root/CutSig.C+");
-  gROOT->ProcessLine(".L ./root/CutTree.C+");
-  gROOT->ProcessLine(".L ./root/Err_Frac.C+");
-  gROOT->ProcessLine(".L ./root/MakeVarList.C+");
-  gROOT->ProcessLine(".L ./root/OutputSearch.C+");
-  gROOT->ProcessLine(".L ./root/PhaseSpace.C+");
-  gROOT->ProcessLine(".L ./root/PIDTable.C+");
-  gROOT->ProcessLine(".L ./root/PlotCorrVar.C+");
-  gROOT->ProcessLine(".L ./root/PlotUsedVars.C+");
-  gROOT->ProcessLine(".L ./root/PlotVar.C+");
-  gROOT->ProcessLine(".L ./root/RenameBranch.C+");
-  gROOT->ProcessLine(".L ./root/ROC_Area.C+");
-  gROOT->ProcessLine(".L ./root/sPlot.C+");
-  gROOT->ProcessLine(".L ./root/TISTOS.C+");
-  gROOT->ProcessLine(".L ./root/VarFit.C+");
-  gROOT->ProcessLine(".L ./root/Vodka.C+");
+  gROOT->ProcessLine((".L "+path+"root/AddBranch.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/BDTApply.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/BDTCut.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/BDTMerit.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/BDTTrain.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/CompareVar.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/Compare.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/Create_Cuts.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/CutEff.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/CutSig.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/CutTree.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/Err_Frac.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/MakeVarList.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/OutputSearch.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PhaseSpace.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PIDTable.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PlotCorrVar.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PlotUsedVars.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PlotVar.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/RenameBranch.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/ROC_Area.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/sPlot.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/TISTOS.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/VarFit.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/Vodka.C+").c_str());
 }
