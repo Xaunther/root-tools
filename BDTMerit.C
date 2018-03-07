cc6973e4c0c3b4bcc5f9fa84f6a806bfbfb28222
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-03-07 10:27:27 +0100

    Spit out bkg rejection

--- BDTMerit.C
+++ BDTMerit.C
@@ -32,8 +32,7 @@ void BDTMerit(RunNumber run_number, double init_value, double final_value, int s
   TTree* MCtree = (TTree*)MCfile->Get("DecayTree");
   string HMcut = "B_M > 5600";
   string LMcut = "B_M < 5000";
-  int N0_MC = MCtree->GetEntries();
-  int N_HM, N_LM;
+  int N0_MC, N_HM, N_LM, N0_bkg;
   double* N_bkg = new double[steps+1];
   double* N_MC = new double[steps+1];
   int* N_MC_raw = new int[steps+1];
@@ -47,6 +46,25 @@ void BDTMerit(RunNumber run_number, double init_value, double final_value, int s
   //Run number cut to look only at certain part of the data
   string runcut = GetRunCut(run_number, "data");
   string runcut_MC = GetRunCut(run_number, "MC");
+  //Initial values
+  N0_MC = MCtree->GetEntries(runcut_MC.c_str());
+  N_HM = datatree->GetEntries((runcut + " && " + HMcut).c_str());
+  N_LM = datatree->GetEntries((runcut + " && " + LMcut).c_str());
+
+  //Interpolate from sidebands
+  if(N_HM != 0 && N_LM != 0 && N_HM != N_LM)
+    {
+      N0_bkg = double(N_LM)*(1-pow(double(N_LM)/double(N_HM),(const_list.xmin-const_list.xmax)/(const_list.xHM-const_list.xmin)))/(1-pow(double(N_LM)/double(N_HM),(const_list.xmin-const_list.xLM)/(const_list.xHM-const_list.xmin)));
+    }
+  else if(N_HM != 0 && N_LM != 0)
+    {
+      N0_bkg = N_LM/double(const_list.xLM-const_list.xmin)*(const_list.xmax-const_list.xmin);
+    }
+  else
+    {
+      N0_bkg = N_HM + N_LM;
+    }
+
   for(int i=0;i<=steps;i++)
     {
       ss_HM << runcut << " && " << HMcut << " && BDT_response > " << init_value+double(i*(final_value-init_value))/double(steps);
@@ -101,11 +119,11 @@ void BDTMerit(RunNumber run_number, double init_value, double final_value, int s
     }
   
   //Output the values
-  cout << "  Cut       Merit         Eff     N_sig   N_bkg   N_MC " << endl;
-  cout << "-------|--------------|---------|-------|-------|------" << endl;
+  cout << "  Cut       Merit       Eff (S)  1-Eff (B)   N_sig   N_bkg   N_MC " << endl;
+  cout << "-------|--------------|---------|---------|-------|-------|------" << endl;
   for(int i=0;i<=steps;i++)
     {
-      cout << setfill(' ') << setw(7) << init_value+double(i*(final_value-init_value))/double(steps) << "|" << setw(14) << sig[i] << "|" << setw(9) << eff[i] << "|" << setw(7) << N_MC[i] << "|" << setw(7) << N_bkg[i] << "|" << setw(6) << N_MC_raw[i] << endl;
+      cout << setfill(' ') << setw(7) << init_value+double(i*(final_value-init_value))/double(steps) << "|" << setw(14) << sig[i] << "|" << setw(9) << eff[i] << "|" << setw(9) << 1-N_bkg[i]/N0_bkg << "|" << setw(7) << N_MC[i] << "|" << setw(7) << N_bkg[i] << "|" << setw(6) << N_MC_raw[i] << endl;
     }
   //Output the line with max significance (saves time!)
   maxpos = GetMaxPos(sig, steps+1);
