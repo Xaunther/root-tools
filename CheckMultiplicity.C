3d09e5f048f8747f4e5640774749070e7e795b65
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-05-31 15:38:35 +0200

    Fix bug and be more speaky

--- CheckMultiplicity.C
+++ CheckMultiplicity.C
@@ -30,16 +30,26 @@ void CheckMultiplicity(string filedir, string cutfile = "")
       if(!wasadded)
 	{
 	  id_evtnumber[N] = evtnumber;
+	  repeated_evtnumber[N]++;
 	  N++;
 	}
 
     }
   int N_repeated = 0;
+  int* nCandidates = new int[N]; for(int i=0;i<N;i++){nCandidates[i]=0;}
   for(int i=0;i<N;i++)
     {
       if(repeated_evtnumber[i] > 1)
 	{
 	  N_repeated++;
+	}
+      nCandidates[repeated_evtnumber[i]-1]++;
+    }
+  for(int i=0;i<N;i++)
+    {
+      if(nCandidates[i]>0)
+	{
+	  cout << i+1 << "  |  " << nCandidates[i] << endl;
 	}
     }
   cout << "Found " << N_repeated << " events out of " << N << "  with multiplicity higher than 1 " << endl;
