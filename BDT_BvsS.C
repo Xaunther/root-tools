8d3fef16f00380c25020f543c9e229e5d584b07a
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-05-31 11:41:49 +0200

    Configure logY from dictionaries

--- BDT_BvsS.C
+++ BDT_BvsS.C
@@ -3,12 +3,15 @@
 #include "TCanvas.h"
 #include "TStyle.h"
 #include <string>
+#include "../Dictionaries/Constants.h"
 using namespace std;
 
 void BDT_BvsS(string filename, double yfactor = 1, string output = "plots/BDT_BvsS.pdf")
 {
   TFile* file = TFile::Open(filename.c_str());
   TH1D* histo = (TH1D*)file->Get("Method_BDT/BDT/MVA_BDT_effBvsS");
+
+  Constants const_list(GetValueFor("Project_name", "Dictionaries/Project_variables.txt"));
 
   for(int i=0;i<histo->GetNbinsX()+1;i++)
     {
@@ -21,7 +24,7 @@ void BDT_BvsS(string filename, double yfactor = 1, string output = "plots/BDT_Bv
 
   //Canvas
   TCanvas* c1 = new TCanvas();
-  c1->SetLogy();
+  c1->SetLogy(const_list.logY);
   c1->SetGrid();
 
   //Draw and save
