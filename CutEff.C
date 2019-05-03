748b93b5753ec72544190763062e0b51f9acfe10
aalfonso <alejandro.alfonso.albero@cern.ch>
2019-05-03 12:15:27 +0200

    Take weights into account when computing cuteff

--- CutEff.C
+++ CutEff.C
@@ -16,7 +16,7 @@
 #include "../Functions/StringTools.h"
 using namespace std;
 
-void CutEff(string dirfile, string cutfile, string precutfile = "", string outfile = "CutEff_results.txt")
+void CutEff(string dirfile, string cutfile, string precutfile = "", string outfile = "CutEff_results.txt", string weight = "")
 {
   string allcuts = GetCuts(cutfile);
   string allprecuts = GetCuts(precutfile);
@@ -36,13 +36,39 @@ void CutEff(string dirfile, string cutfile, string precutfile = "", string outfi
   //Data chain
   TChain* chain = GetChain(dirfile);
 
+  //If weights are requested
+  double* w_f = new double[N_cuts+1];
+  double w_0 = 1.;
+  if(weight != "")
+    {
+      w_0 = GetMean(dirfile, weight);
+    }
   //Simply compute #of evts before and after
   N0 = chain->GetEntries(allprecuts.c_str());
   for(int i=0;i<N_cuts;i++)
     {
       N_final[i] = chain->GetEntries((allprecuts+" && "+cuts[i]).c_str());
+      //If weights are requested, we need the mean after the cuts
+      if(weight != "")
+	{
+	  w_f[i] = GetMean(dirfile, weight, "", allprecuts+" && "+cuts[i]);
+	}
+      //Otherwise, just 1
+      else
+	{
+	  w_f[i] = 1.;
+	}
     }
   N_final[N_cuts] =chain->GetEntries((allprecuts+" && "+allcuts).c_str());
+  if(weight != "")
+    {
+      w_f[N_cuts] = GetMean(dirfile, weight, "", allprecuts+" && "+allcuts);
+    }
+  //Otherwise, just 1
+  else
+    {
+      w_f[N_cuts] = 1.;
+    }
   //Now, produce a gorgeous output #4dalulz
   ofstream fout;
   fout.open(outfile.c_str());
@@ -60,9 +86,9 @@ void CutEff(string dirfile, string cutfile, string precutfile = "", string outfi
   int maxL = GetMaxLength(cuts, N_cuts);
   for(int i=0;i<N_cuts;i++)
     {
-      fout << cuts[i] << setw(maxL+5-int(cuts[i].size())) << "  |  " << N_final[i]/double(N0) << endl;
+      fout << cuts[i] << setw(maxL+5-int(cuts[i].size())) << "  |  " << (w_f[i]/w_0)*(N_final[i]/double(N0)) << endl;
     }
-  fout << "Global" << setw(maxL+5-6) << "  |  " << N_final[N_cuts]/double(N0) << endl;
+  fout << "Global" << setw(maxL+5-6) << "  |  " << (w_f[N_cuts]/w_0)*(N_final[N_cuts]/double(N0)) << endl;
   fout.close();
 
 }
