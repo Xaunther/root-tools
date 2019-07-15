14171f87d46a4d03cb6713fbb407c49b0e40c025
aalfonso <alejandro.alfonso.albero@cern.ch>
2019-07-15 10:18:55 +0200

    Added script to recompute ROC_Area fom BDT results ntuple

--- ROC_Area.C
+++ ROC_Area.C
@@ -1,38 +1,19 @@
+//Script that reads histogram and computes area.
+//Designed to compute ROC area from ROC Curve histogram from TMVA
 #include <iostream>
-#include <iomanip>
-#include <fstream>
 #include <string>
-#include <sstream>
-#include "TTree.h"
+#include "TH1D.h"
 #include "TFile.h"
 using namespace std;
 
-void ROC_Area(string treename, int steps = 100, string filename = "Tuples/BDT-results_OP.root")
+void ROC_Area(string filename, string histname = "default/Method_BDT/BDT/MVA_BDT_trainingRejBvsS")
 {
   TFile* file = new TFile(filename.c_str());
-  TTree* tree = (TTree*)file->Get(treename.c_str());
-
-  int N_sig_init = tree->GetEntries("classID==0");
-  int N_bkg_init = tree->GetEntries("classID==1");
-
-  double x0 = -1.;
-  double xf = 1.;
-  double w = (xf-x0)/double(steps);
+  TH1D* hist = (TH1D*)file->Get(histname.c_str());
   double area = 0;
-  for(int i=0;i<steps;i++)
+  for(int i=1;i<=hist->GetEntries();i++)
     {
-      stringstream cut0, cutf;
-      cut0 << "BDT > " << x0 + w*i;
-      cutf << "BDT > " << x0 + w*(i+1);
-      
-      int N_sig0 = tree->GetEntries(("classID==0 && " + cut0.str()).c_str());
-      int N_sigf = tree->GetEntries(("classID==0 && " + cutf.str()).c_str());
-      int N_bkg0 = tree->GetEntries(("classID==1 && " + cut0.str()).c_str());
-      int N_bkgf = tree->GetEntries(("classID==1 && " + cutf.str()).c_str());
-
-      area += (double(-N_sigf+N_sig0)/double(N_sig_init))*(2-double(N_bkgf)/double(N_bkg_init)-double(N_bkg0)/double(N_bkg_init))/2.;
-      cut0.str("");
-      cutf.str("");
+      area += hist->GetBinWidth(i)*hist->GetAt(i);
     }
   
   cout << endl << "Area of the curve: " << area << endl << endl;
