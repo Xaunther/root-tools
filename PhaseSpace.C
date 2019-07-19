ebef89c0ed3637726849653efda905b80e3fe87a
aalfonso <alejandro.alfonso.albero@cern.ch>
2019-07-19 09:47:11 +0200

    Add mass sub for Gauss generated ntuples

--- PhaseSpace.C
+++ PhaseSpace.C
@@ -1,15 +1,18 @@
 //This used to be some crappy code I assembled long ago... USELESS PIECE OF SH
 //So, I will try to generate some distribution, which mihgt be useful to understand some dependency of the efficiencies on the mass of the resonance. I'll see
+//Added a new option to obtain Lambda_b momentum distribution from an ntuple
 #include <iostream>
 #include "TGenPhaseSpace.h"
 #include "TLorentzVector.h"
 #include "TH1F.h"
 #include "TTree.h"
+#include "TChain.h"
 #include "TFile.h"
+#include "../Functions/TreeTools.h"
 #include "../Functions/StringTools.h"
 using namespace std;
 
-void PhaseSpace(double mothermass, string daughtermasses, string pnames, string outfilename, const int N_evts = 100000)
+void PhaseSpace(double mothermass, string daughtermasses, string pnames, string outfilename, int N_evts = 100000, string mother_P_filename = "", string mother_P_prefix = "B_P")
 {
   //Masses involved (GeV)
   int N_daughters = 0;
@@ -27,13 +30,13 @@ void PhaseSpace(double mothermass, string daughtermasses, string pnames, string 
   axname[0] = "PX";
   axname[1] = "PY";
   axname[2] = "PZ";
+
   //////////////////////////////////////
 
   //Decaying particle
   TLorentzVector mother(0., 0., 0., mothermass);
   //Set up the decay
   TGenPhaseSpace event;
-  double caca[3] = {1.,1.,0.};
   event.SetDecay(mother, N_daughters, d_mass);
 
   //Set up the ntuple
@@ -57,9 +60,33 @@ void PhaseSpace(double mothermass, string daughtermasses, string pnames, string 
     }
   double weight;
   TBranch* br_weight = tree->Branch("evt_weight", &weight, "evt_weight/D");
-  //Generate events, izy. Save them in tree
+
+  //Set up mother momentum
+  TChain* mother_P_chain;
+  double* mother_P = new double[3];
+  if(mother_P_filename != "")
+    {
+      mother_P_chain = GetChain(mother_P_filename);
+      mother_P_chain->SetBranchAddress((mother_P_prefix+"X").c_str(), &mother_P[0]);
+      mother_P_chain->SetBranchAddress((mother_P_prefix+"Y").c_str(), &mother_P[1]);
+      mother_P_chain->SetBranchAddress((mother_P_prefix+"Z").c_str(), &mother_P[2]);
+    }
+  //Generate events, izy. Save them in tree. Can only generate either the minimum of the number requested and the events in the mother P distribution
+  if(mother_P_filename != "")
+    {
+      if(mother_P_chain->GetEntries()<N_evts)
+	{
+	  N_evts = mother_P_chain->GetEntries();
+	}
+    }
   for (int n=0;n<N_evts;n++)
     {
+      //Re-roll momentum
+      if(mother_P_filename != "")
+	{
+	  mother_P_chain->GetEntry(n);
+	  mother.SetXYZM(mother_P[0], mother_P[1], mother_P[2], mothermass);
+	}
       weight = event.Generate();
       for(int i=0;i<N_daughters;i++)
 	{
