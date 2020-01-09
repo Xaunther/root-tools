14e9b50488173d3c6c61d7c95a38abfe5d428f6c
aalfonso <alejandro.alfonso.albero@cern.ch>
2020-01-09 09:51:12 +0100

    Add PIDMatrix to rootinit

--- rootinit.C
+++ rootinit.C
@@ -70,6 +70,7 @@ void rootinit(std::string path)
   gROOT->ProcessLine((".L "+path+"root/MassSub.C+").c_str());
   gROOT->ProcessLine((".L "+path+"root/OutputSearch.C+").c_str());
   gROOT->ProcessLine((".L "+path+"root/PhaseSpace.C+").c_str());
+  gROOT->ProcessLine((".L "+path+"root/PIDMatrix.C+").c_str());
   gROOT->ProcessLine((".L "+path+"root/PIDPunzi.C+").c_str());
   gROOT->ProcessLine((".L "+path+"root/PIDTable.C+").c_str());
   gROOT->ProcessLine((".L "+path+"root/plot2D.C+").c_str());
