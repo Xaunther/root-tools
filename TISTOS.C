44a5ea86e211a623a49a20b081cca9ba7984b97a
Alejandro Alfonso Albero <alejandro.alfonso.albero@cern.ch>
2020-02-25 11:53:44 +0100

    Update TISTOS script

--- TISTOS.C
+++ TISTOS.C
@@ -1,64 +1,47 @@
+//Trigger efficiency, assuming one is accepting TOS events
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-#include "TTree.h"
 #include "TChain.h"
-#include "TH1F.h"
-#include "TFile.h"
-#include "TCanvas.h"
-#include "TLeaf.h"
 #include "../Functions/TISTOS.h"
 #include "../Functions/Dictreading.h"
 #include "../Functions/Filereading.h"
+#include "../Functions/TreeTools.h"
+#include "../Functions/ArrayTools.h"
 using namespace std;
 
 void TISTOS(string dirfile, string cutsfilename, string outfile = "TISTOS_results.txt", string L0_lines = "Variables/L0_TISTOS.txt", string Hlt1_lines = "Variables/Hlt1_TISTOS.txt", string Hlt2_lines = "Variables/Hlt2_TISTOS.txt")
 {
+  //Obtain cuts (in case some cuts must be applied before TISTOSing)
   string cuts = GetCuts(cutsfilename);
-  cout << cuts << endl;
 
-  int N_files = 0;
-  string* filenames = ReadVariables(N_files, dirfile);
+  //Get chain
+  TChain* chain = GetChain(dirfile);
 
   //Trigger lines used for TISTOS
   int N_L0 = 0, N_Hlt1 = 0, N_Hlt2 = 0;
   string* L0_list = ReadVariables(N_L0, L0_lines);
   string* Hlt1_list = ReadVariables(N_Hlt1, Hlt1_lines);
   string* Hlt2_list = ReadVariables(N_Hlt2, Hlt2_lines);
+  //Clean suffixes
+  L0_list = CleanTISTOS(L0_list, N_L0);
+  Hlt1_list = CleanTISTOS(Hlt1_list, N_Hlt1);
+  Hlt2_list = CleanTISTOS(Hlt2_list, N_Hlt2);
   //Perform TIS and TISTOS Cuts
   string L0_TIS = MakeTIS(L0_list, N_L0);
-  string L0_TOS = MakeTOS(L0_list, N_L0);
   string L0_TISTOS = MakeTISTOS(L0_list, N_L0);
-  string L0_TISorTOS = MakeTISorTOS(L0_list, N_L0);
   string Hlt1_TIS = MakeTIS(Hlt1_list, N_Hlt1);
-  string Hlt1_TOS = MakeTOS(Hlt1_list, N_Hlt1);
   string Hlt1_TISTOS = MakeTISTOS(Hlt1_list, N_Hlt1);
-  string Hlt1_TISorTOS = MakeTISorTOS(Hlt1_list, N_Hlt1);
   string Hlt2_TIS = MakeTIS(Hlt2_list, N_Hlt2);
-  string Hlt2_TOS = MakeTOS(Hlt2_list, N_Hlt2);
   string Hlt2_TISTOS = MakeTISTOS(Hlt2_list, N_Hlt2);
-  string Hlt2_TISorTOS = MakeTISorTOS(Hlt2_list, N_Hlt2);
-
-  //Data chain
-  string treename = GetTreeName(dirfile);
-  cout << "Reading Tree " << treename << endl;
-  TChain* chain = new TChain(treename.c_str());
-
-  //Add to chain and get N of entries
-  for (int i = 0; i < N_files; i++)
-  {
-    chain->Add(filenames[i].c_str());
-  }
-
-  //Cut chain into new TChain in a temp root file
-  TFile* cutfile = new TFile("Tuples/tmp.root", "recreate");
-  TTree* cuttree = (TTree*)chain->CopyTree(cuts.c_str());
-  cutfile->Write();
 
   //Part where the output is stored into outfile
+  //We compute L0 efficiency
+  //Then, Hlt1 efficiency, having passed L0
   ofstream fout;
-  int N_TIS, N_TOS, N_TISTOS, N_TISorTOS;
+  double eff_TIS, eff_TISTOS;
 
   fout.open(outfile.c_str());
   fout << "TISTOS method applied on tuples defined at " << dirfile << ":" << endl << endl;
@@ -71,14 +54,16 @@ void TISTOS(string dirfile, string cutsfilename, string outfile = "TISTOS_result
   }
   fout << endl;
   //Compute Numbers
-  N_TIS = cuttree->GetEntries(L0_TIS.c_str());
-  N_TISTOS = cuttree->GetEntries(L0_TISTOS.c_str());
-  N_TISorTOS = cuttree->GetEntries(L0_TISorTOS.c_str());
-  N_TOS = cuttree->GetEntries(L0_TOS.c_str());
+  eff_TIS = GetMean(chain, L0_TIS, cuts);
+  eff_TISTOS = GetMean(chain, L0_TISTOS, cuts);
 
-  fout << "Trigger eff: " << double(N_TISTOS) / N_TIS << endl;
+  fout << "L0_Trigger_eff = " << eff_TISTOS / eff_TIS << endl;
   fout << endl;
+
+
   //Hlt1
+  //Include L0 cuts into cuts
+  cuts = cuts + " * (" + MakeTOS(L0_list, N_L0) + ")";
   fout << "        Hlt1 lines used        " << endl;
   fout << "-------------------------------" << endl;
   for (int i = 0; i < N_Hlt1; i++)
@@ -87,14 +72,16 @@ void TISTOS(string dirfile, string cutsfilename, string outfile = "TISTOS_result
   }
   fout << endl;
   //Compute numbers
-  N_TIS = cuttree->GetEntries(("(" + L0_TIS + ") && (" + Hlt1_TIS + ")").c_str());
-  N_TISTOS = cuttree->GetEntries(("(" + L0_TISTOS + ") && (" + Hlt1_TISTOS + ")").c_str());
-  N_TISorTOS = cuttree->GetEntries(("(" + L0_TISorTOS + ") && (" + Hlt1_TISorTOS + ")").c_str());
-  N_TOS = cuttree->GetEntries(("(" + L0_TOS + ") && (" + Hlt1_TOS + ")").c_str());
+  eff_TIS = GetMean(chain, Hlt1_TIS, cuts);
+  eff_TISTOS = GetMean(chain, Hlt1_TISTOS, cuts);
 
-  fout << "Trigger eff: " << double(N_TISTOS) / N_TIS << endl;
+  fout << "Hlt1_Trigger_eff = " << eff_TISTOS / eff_TIS << endl;
   fout << endl;
+
+
   //Hlt2
+  //Include Hlt1 cuts into cuts
+  cuts = cuts + " * (" + MakeTOS(Hlt1_list, N_Hlt1) + ")";
   fout << "        Hlt2 lines used        " << endl;
   fout << "-------------------------------" << endl;
   for (int i = 0; i < N_Hlt2; i++)
@@ -103,21 +90,14 @@ void TISTOS(string dirfile, string cutsfilename, string outfile = "TISTOS_result
   }
   fout << endl;
   //Compute numbers
-  N_TIS = cuttree->GetEntries(("(" + L0_TIS + ") && (" + Hlt1_TIS + ") && (" + Hlt2_TIS + ")").c_str());
-  N_TISTOS = cuttree->GetEntries(("(" + L0_TISTOS + ") && (" + Hlt1_TISTOS + ") && (" + Hlt2_TISTOS + ")").c_str());
-  N_TISorTOS = cuttree->GetEntries(("(" + L0_TISorTOS + ") && (" + Hlt1_TISorTOS + ") && (" + Hlt2_TISorTOS + ")").c_str());
-  N_TOS = cuttree->GetEntries(("(" + L0_TOS + ") && (" + Hlt1_TOS + ") && (" + Hlt2_TOS + ")").c_str());
+  eff_TIS = GetMean(chain, Hlt2_TIS, cuts);
+  eff_TISTOS = GetMean(chain, Hlt2_TISTOS, cuts);
 
-  fout << "Trigger eff: " << double(N_TISTOS) / N_TIS << endl;
-  fout << endl;
-  cout << "N_TIS: "      << N_TIS      << endl;
-  cout << "N_TISTOS: "   << N_TISTOS   << endl;
-  cout << "N_TISorTOS: " << N_TISorTOS << endl;
-  cout << "N_TOS: "      << N_TOS      << endl;
+  fout << "Hlt2_Trigger_eff = " << eff_TISTOS / eff_TIS << endl;
+
 
   fout.close();
-
-  cutfile->Close();
+  CloseChain(chain);
 }
 
 #if !defined(__CLING__)
