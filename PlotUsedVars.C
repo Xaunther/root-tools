5e3dc7ec2d7714a1690c71af946619eb95005040
Xaunther <alex.1993.24@gmail.com>
2020-06-12 16:49:31 +0200

    Update script to plot list of variables

--- PlotUsedVars.C
+++ PlotUsedVars.C
@@ -13,43 +13,43 @@
 #include "../Functions/Filereading.h"
 #include "../Functions/TreeTools.h"
 using namespace std;
-void PlotUsedVars(string cutfile = "Variables/Cuts.txt", string plotopt = "", string tupledir = "tuples.dir")
+void PlotUsedVars(string varlist, string tupledir, string cutfile = "Variables/Cuts.txt", string plotopt = "", string prefix = "")
 {
   int N_vars = 0;
-  string* varnames = ReadVariables(N_vars, "Variables/VariablesUsed.txt");
+  string *varnames = ReadVariables(N_vars, varlist);
 
   string cuts = GetCuts(cutfile);
 
   //Data chain
-  TChain* chain = GetChain(tupledir);
+  TChain *chain = GetChain(tupledir);
 
-  TCanvas* c1 = new TCanvas();
+  TCanvas *c1 = new TCanvas();
   for (int i = 0; i < N_vars; i++)
   {
     chain->Draw(varnames[i].c_str(), cuts.c_str(), plotopt.c_str());
-    c1->SaveAs((varnames[i] + ".pdf").c_str());
+    c1->SaveAs((prefix + varnames[i] + ".pdf").c_str());
   }
   c1->Close();
 }
 
 #if !defined(__CLING__)
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   switch (argc - 1)
   {
-    case 0:
-    PlotUsedVars();
-    break;
-    case 1:
-    PlotUsedVars(*(new string(argv[1])));
-    break;
-    case 2:
+  case 2:
     PlotUsedVars(*(new string(argv[1])), *(new string(argv[2])));
     break;
-    case 3:
+  case 3:
     PlotUsedVars(*(new string(argv[1])), *(new string(argv[2])), *(new string(argv[3])));
     break;
-    default:
+  case 4:
+    PlotUsedVars(*(new string(argv[1])), *(new string(argv[2])), *(new string(argv[3])), *(new string(argv[4])));
+    break;
+  case 5:
+    PlotUsedVars(*(new string(argv[1])), *(new string(argv[2])), *(new string(argv[3])), *(new string(argv[4])), *(new string(argv[5])));
+    break;
+  default:
     cout << "Wrong number of arguments (" << argc << ") for " << argv[0] << endl;
     return (1);
     break;
