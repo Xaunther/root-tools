5c875606122be03fbc2f8eb29ef828b2612112af
aalfonso <alejandro.alfonso.albero@cern.ch>
2021-07-06 11:55:16 +0200

    Autoformat

--- Functions/Fits.cxx
+++ Functions/Fits.cxx
@@ -35,10 +35,10 @@
 
 using namespace std;
 
-FitFunction* FitFunction_init()
+FitFunction *FitFunction_init()
 {
 	//I create an array of pointers to the fitting functions, so no need to change code in VarFit.C if we add more ;)
-	FitFunction* fitf = new FitFunction[18];
+	FitFunction *fitf = new FitFunction[18];
 	fitf[0] = FitGauss_Exp;
 	fitf[1] = FitGauss_Exp_Line;
 	fitf[2] = FitDoubleCB_Exp;
@@ -60,7 +60,7 @@ FitFunction* FitFunction_init()
 	return fitf;
 }
 
-RooWorkspace* FitGauss_Exp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitGauss_Exp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -89,7 +89,7 @@ RooWorkspace* FitGauss_Exp(string variablename, TTree* chain, string w_var, stri
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitGauss_Exp_Line(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitGauss_Exp_Line(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -107,7 +107,7 @@ RooWorkspace* FitGauss_Exp_Line(string variablename, TTree* chain, string w_var,
 	RooGaussian signal(name_list.comppdf[0].c_str(), name_list.comppdf[0].c_str(), B_M, mean, width);
 
 	//Exponential
-	RooRealVar bkgpar(name_list.exp_par[0].c_str(), name_list.exp_par[0].c_str(), 4.*const_list.bkgpar_0, const_list.bkgpar_min, const_list.bkgpar_max);
+	RooRealVar bkgpar(name_list.exp_par[0].c_str(), name_list.exp_par[0].c_str(), 4. * const_list.bkgpar_0, const_list.bkgpar_min, const_list.bkgpar_max);
 	bkgpar.setConstant(!(const_list.bkgpar_min - const_list.bkgpar_max));
 	RooExponential bkg(name_list.comppdf[1].c_str(), name_list.comppdf[1].c_str(), B_M, bkgpar);
 
@@ -118,14 +118,14 @@ RooWorkspace* FitGauss_Exp_Line(string variablename, TTree* chain, string w_var,
 
 	RooRealVar fsig(name_list.fcomp[0].c_str(), name_list.fcomp[0].c_str(), entries * const_list.fsig_0, 0, entries);
 	RooRealVar fbkg(name_list.fcomp[1].c_str(), name_list.fcomp[1].c_str(), entries * const_list.fbkg_0, 0, entries);
-	RooRealVar fbkg2(name_list.fcomp[2].c_str(), name_list.fcomp[2].c_str(), entries * const_list.fbkg_0, 0, entries);
+	RooRealVar fbkg2(name_list.fcomp[2].c_str(), name_list.fcomp[2].c_str(), entries * (1 - const_list.fsig_0 - const_list.fbkg_0), 0, entries);
 
 	RooAddPdf model(name_list.pdfmodel[0].c_str(), name_list.pdfmodel[0].c_str(), RooArgList(signal, bkg, bkg2), RooArgList(fsig, fbkg, fbkg2));
 
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitDoubleCB_Exp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitDoubleCB_Exp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -162,7 +162,7 @@ RooWorkspace* FitDoubleCB_Exp(string variablename, TTree* chain, string w_var, s
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitGauss(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitGauss(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -182,7 +182,7 @@ RooWorkspace* FitGauss(string variablename, TTree* chain, string w_var, string* 
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitCB(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitCB(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -207,7 +207,7 @@ RooWorkspace* FitCB(string variablename, TTree* chain, string w_var, string* act
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitDoubleCB(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitDoubleCB(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -234,7 +234,7 @@ RooWorkspace* FitDoubleCB(string variablename, TTree* chain, string w_var, strin
 
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
-RooWorkspace* FitCBExp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitCBExp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -259,7 +259,7 @@ RooWorkspace* FitCBExp(string variablename, TTree* chain, string w_var, string* 
 
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
-RooWorkspace* FitGaussExp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitGaussExp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -281,7 +281,7 @@ RooWorkspace* FitGaussExp(string variablename, TTree* chain, string w_var, strin
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitDoubleGaussExp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitDoubleGaussExp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -305,7 +305,7 @@ RooWorkspace* FitDoubleGaussExp(string variablename, TTree* chain, string w_var,
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitDoubleGaussExp_Exp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitDoubleGaussExp_Exp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -338,7 +338,7 @@ RooWorkspace* FitDoubleGaussExp_Exp(string variablename, TTree* chain, string w_
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitCBExp_Exp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitCBExp_Exp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -373,7 +373,7 @@ RooWorkspace* FitCBExp_Exp(string variablename, TTree* chain, string w_var, stri
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitArgusGauss(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitArgusGauss(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -398,7 +398,7 @@ RooWorkspace* FitArgusGauss(string variablename, TTree* chain, string w_var, str
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitB2KstIsoG(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitB2KstIsoG(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -435,7 +435,7 @@ RooWorkspace* FitB2KstIsoG(string variablename, TTree* chain, string w_var, stri
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitNothing(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitNothing(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -444,24 +444,24 @@ RooWorkspace* FitNothing(string variablename, TTree* chain, string w_var, string
 	Activate(chain, act_vars, N);
 	chain->SetBranchStatus(variablename.c_str(), 1);
 	//Workspace creation
-	RooWorkspace* ws = new RooWorkspace(name_list.workspace.c_str());
+	RooWorkspace *ws = new RooWorkspace(name_list.workspace.c_str());
 
 	double xmin = const_list.xmin;
 	double xmax = const_list.xmax;
 	RooRealVar B_M(variablename.c_str(), variablename.c_str(), xmin, xmax);
 	//Initialize all the stuff needed for the fit
-	RooArgSet* args;
-	RooRealVar* wvar;
+	RooArgSet *args;
+	RooRealVar *wvar;
 	if (w_var != "")
-	  {
-	    wvar = new RooRealVar(w_var.c_str(), w_var.c_str(), 0, 10000);
-	    args = new RooArgSet(B_M, *wvar);
-	    chain->SetBranchStatus(w_var.c_str(), 1);
-	  }
+	{
+		wvar = new RooRealVar(w_var.c_str(), w_var.c_str(), 0, 10000);
+		args = new RooArgSet(B_M, *wvar);
+		chain->SetBranchStatus(w_var.c_str(), 1);
+	}
 	else
-	  {
-	    args = new RooArgSet(B_M);
-	  }
+	{
+		args = new RooArgSet(B_M);
+	}
 	RooDataSet data("data", "data", chain, *args, 0, w_var.c_str());
 	RooRealVar N_comps(name_list.N_comps.c_str(), name_list.N_comps.c_str(), 0., 0., 0.);
 	ws->import(data);
@@ -470,7 +470,7 @@ RooWorkspace* FitNothing(string variablename, TTree* chain, string w_var, string
 	return ws;
 }
 
-void MultiPlot(string variablename, int N_trees, TTree** chain, string opts)
+void MultiPlot(string variablename, int N_trees, TTree **chain, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -496,7 +496,7 @@ void MultiPlot(string variablename, int N_trees, TTree** chain, string opts)
 	int colours[] = {kRed, kBlue, kGreen, kMagenta, kCyan, kYellow, kBlack, kMagenta + 2, kCyan + 2, kYellow + 2};
 
 	//Create mah canvas
-	TCanvas* c = new TCanvas();
+	TCanvas *c = new TCanvas();
 	c->cd(0)->SetTicks(1, 1);
 	c->SetTitle(title.c_str());
 
@@ -513,19 +513,17 @@ void MultiPlot(string variablename, int N_trees, TTree** chain, string opts)
 			chain[i]->Draw(variablename.c_str(), "", (hist_opts + " SAME").c_str());
 		}
 		//Retrieve histo
-		TH1* hist1 = chain[i]->GetHistogram();
+		TH1 *hist1 = chain[i]->GetHistogram();
 		//Set format
 		hist1->SetFillColorAlpha(colours[i], 0.5);
 		hist1->SetLineColor(colours[i]);
 		hist1->SetFillStyle(1001);
-
 	}
 
 	//Axis titles
-	TH1F* h1 = (TH1F*)gPad->GetPrimitive("h1");
+	TH1F *h1 = (TH1F *)gPad->GetPrimitive("h1");
 	h1->SetTitle(title.c_str());
 	h1->SetStats(0);
-
 
 	//If logarithm is requested
 	if (const_list.logY)
@@ -561,10 +559,9 @@ void MultiPlot(string variablename, int N_trees, TTree** chain, string opts)
 	*/
 	string pdfname = "plots/" + variablename + "_Compare.pdf";
 	c->SaveAs(pdfname.c_str());
-
 }
 
-RooWorkspace* FitArgus_Gauss_Exp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitArgus_Gauss_Exp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -606,7 +603,7 @@ RooWorkspace* FitArgus_Gauss_Exp(string variablename, TTree* chain, string w_var
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitExp(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitExp(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -624,7 +621,7 @@ RooWorkspace* FitExp(string variablename, TTree* chain, string w_var, string* ac
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitLine(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitLine(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -642,7 +639,7 @@ RooWorkspace* FitLine(string variablename, TTree* chain, string w_var, string* a
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* FitExp_Line(string variablename, TTree* chain, string w_var, string* act_vars, int N, string opts)
+RooWorkspace *FitExp_Line(string variablename, TTree *chain, string w_var, string *act_vars, int N, string opts)
 {
 	//Initialize constants
 	Constants const_list(opts);
@@ -670,20 +667,20 @@ RooWorkspace* FitExp_Line(string variablename, TTree* chain, string w_var, strin
 	return DoFit(B_M, chain, model, const_list, name_list, variablename, w_var);
 }
 
-RooWorkspace* DoFit(RooRealVar B_M, TTree* chain, RooAbsPdf& model, Constants& const_list, Names& name_list, string variablename, string w_var)
+RooWorkspace *DoFit(RooRealVar B_M, TTree *chain, RooAbsPdf &model, Constants &const_list, Names &name_list, string variablename, string w_var)
 {
-	RooArgSet* args;
-	RooWorkspace* ws = new RooWorkspace(name_list.workspace.c_str());
-	RooRealVar* wvar;
+	RooArgSet *args;
+	RooWorkspace *ws = new RooWorkspace(name_list.workspace.c_str());
+	RooRealVar *wvar;
 	if (w_var != "")
 	{
-	  wvar = new RooRealVar(w_var.c_str(), w_var.c_str(), 0, 10000);
-	  args = new RooArgSet(B_M, *wvar);
-	  chain->SetBranchStatus(w_var.c_str(), 1);
+		wvar = new RooRealVar(w_var.c_str(), w_var.c_str(), 0, 10000);
+		args = new RooArgSet(B_M, *wvar);
+		chain->SetBranchStatus(w_var.c_str(), 1);
 	}
 	else
 	{
-	  args = new RooArgSet(B_M);
+		args = new RooArgSet(B_M);
 	}
 	RooDataSet data(name_list.dataset.c_str(), name_list.dataset.c_str(), chain, *args, 0, w_var.c_str());
 	model.fitTo(data, RooFit::SumW2Error(const_list.SumW2Error));
